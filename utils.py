f7f45b5c6aabfd8f4a918c73123af80662d3616e
wahdia <wahabdiallo551@gmail.com>
2024-12-02 15:27:27 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def smil_prediction(img, mdl="Image_Smil_classifier.h5"):
 
 # cette fonction attend 2 paramètres
 # un chemin vers une image et un chemin vers un modèle de ML/DL
-def TB_prediction(img, mdl="image_TB_classifier.h5"):
+def TB_prediction(img, mdl="Image_TB_classifier.h5"):
     # je précharge mon modèle
     model = load_model(mdl)
 
